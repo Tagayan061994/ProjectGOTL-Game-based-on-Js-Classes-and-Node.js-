f1352b432087d4f558cf5d72683a7405a3c0694b
yourusername <your@email.com>
2018-12-01 15:32:32 +0400

    changes only classes

---
+++ Grass.js
@@ -0,0 +1,19 @@
+class Grass extends LivingCreature {
+    
+//sharjvuma gtnelov 0 index ov vandakner 
+    mul() {
+        this.multiply++;
+        var emptyCells = this.chooseCell(0);
+        var newCell = random(emptyCells);
+
+        if (newCell && this.multiply >= 3) {
+            var newX = newCell[0];
+            var newY = newCell[1];
+            matrix[newY][newX] = this.index;
+
+            var newGrass = new Grass(newX, newY, this.index);
+            grassArr.push(newGrass);
+            this.multiply = 0;
+        }
+    }
+}
